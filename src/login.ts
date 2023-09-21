defb1fab97ca2829328ef0c49b1c0398ac043ac3
okanay <okanay@hotmail.com>
2023-09-21 10:06:12 +0300

    Some Auth Improve 0.02

--- src/login.ts
+++ src/login.ts
@@ -1,47 +1,46 @@
-import express from 'express';
+import express from "express";
 import jsonwebtoken, { JwtPayload, verify } from "jsonwebtoken";
-
+import jwtDecode from "jwt-decode";
 
 const router = express.Router();
-const jwtKey = process.env.JWT || 'no-key';
+const jwtKey = process.env.JWT || "no-key";
 
-
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   let token = req.headers.authorization;
-  let decode;
+  if (!token) return res.status(401).send("Oturum açmanız gerekiyor");
 
-  if (!token) return res.status(401).send('Oturum açmanız gerekiyor');
+  const decode = jwtDecode(token);
 
-  try
-  {
-    decode = verify(token, jwtKey)
-  }
-  catch (err)
-  {
-    return res.status(401).send('Oturumunuz süresi dolmuş veya token geçersiz');
+  try {
+    verify(token, jwtKey);
+  } catch (err) {
+    return res.status(401).send("Oturumunuz süresi dolmuş veya token geçersiz");
   }
 
   res.status(200).send({
     data: {
       decode,
-      token
+      token,
     },
   });
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
+  // const lastLoginDeviceId = `${req.headers['user-agent']}-${req.headers['accept-language']}`;
+  // console.log(lastLoginDeviceId);
+
   const body = await req.body;
 
   const user = {
-    id: 'randomUserId',
-    password: '1234asd',
+    id: "randomUserId",
+    password: "1234asd",
   };
 
   if (!user || user.password !== body.password) {
-    return res.status(401).send('Kullanıcı adı veya şifre hatalı');
+    return res.status(401).send("Kullanıcı adı veya şifre hatalı");
   }
 
-  const token = jsonwebtoken.sign({ id: user.id }, jwtKey, { expiresIn: '1d' });
+  const token = jsonwebtoken.sign({ id: user.id }, jwtKey, { expiresIn: "1d" });
   return res.status(200).send({ token });
 });
 
