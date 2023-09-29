f2b9bc911cd595a7559de03d2d1cfbac24632fa2
okanay <okanay@hotmail.com>
2023-09-29 15:19:00 +0300

    helmet return

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 import cors from "cors";
 
 import login from "./login";
@@ -11,6 +12,7 @@ const app = express();
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 
 app.get("/", (req, res) => {
   res.json({
